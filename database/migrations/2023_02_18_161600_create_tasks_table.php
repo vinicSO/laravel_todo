dc75d1340863945d3b3c3270f31e370586daef60
Vinícius S. Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-02-18 13:46:29 -0300

    create migration create_tasks_table

---
+++ database/migrations/2023_02_18_161600_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use \App\Models\User;
+use \App\Models\Category;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('description');
+            $table->dateTime('due_date');
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onDelete('CASCADE');
+            $table->foreignIdFor(Category::class)->references('id')->on('categories')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeignIdFor(User::class);
+            $table->dropForeignIdFor(Category::class);
+        });
+        Schema::dropIfExists('tasks');
+    }
+};
