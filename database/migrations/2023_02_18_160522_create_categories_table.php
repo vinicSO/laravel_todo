49f7a6b9f21dbd7368e8d4c044c56e98162358c1
Vinícius S. Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-02-18 13:46:05 -0300

    create migration create_categories_table

---
+++ database/migrations/2023_02_18_160522_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use \App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('color')->default('#ffffff');
+            $table->foreignIdFor(User::class)
+                ->references('id')
+                ->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropForeignIdFor(User::class);
+        });
+        Schema::dropIfExists('categories');
+    }
+};
