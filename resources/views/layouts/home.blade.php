ffbcf0d6cbd1972636f23ec23ef777cbe1618ce1
Vinícius S. Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-02-23 22:47:29 -0300

    initial config to templates

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>TodoApp</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/assets/css/style.css" />
+    </head>
+    <body>
+    </body>
+</html>
