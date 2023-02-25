3c1018ce61d0f6e54c164572bc382aeb44197ff3
Vinícius S. Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-02-24 23:06:11 -0300

    static template finished

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -11,5 +11,71 @@
         <link rel="stylesheet" href="/assets/css/style.css" />
     </head>
     <body>
+        <div class="container">
+            <div class="sidebar">
+                <img src="/assets/images/logo.png">
+            </div>
+
+            <div class="content">
+                <nav>
+                    <a href="#" class="btn btn-primary">Criar Tarefa</a>
+                </nav>
+
+                <main>
+                    <section class="graph">
+                        <div class="graph_header">
+                            <h2>Progresso do Dia</h2>
+                            <div class="graph_header_line"></div>
+                            <div class="graph_header_date">
+                                <img src="assets/images/icon-prev.png">
+
+                                13 de Dez
+
+                                <img src="assets/images/icon-next.png">
+
+                            </div>
+                        </div>
+
+                        <div class="graph_header_subtitle">
+                            Tarefas: <b>3/6</b>
+                        </div>
+
+                        <div class="graph_placeholder">
+
+                        </div>
+
+                        <div class="tasks_left">
+                            <img src="/assets/images/icon-info.png">
+                            <p class="tasks_left_label">Restam 3 tarefas para serem realizadas</p>
+                        </div>
+                    </section>
+
+                    <section class="list">
+                        <div class="list_header">
+                            <select class="list_header_select">
+                                <option value="1">Todas as tarefas</option>
+                            </select>
+                        </div>
+
+                        <div class="task_list">
+                            <div class="task">
+                                <div class="title">
+                                    <input type="checkbox">
+                                    <div class="title_label">Título da tarefa</div>
+                                </div>
+                                <div class="priority">
+                                    <div class="sphere"></div>
+                                    <div>Título da tarefa</div>
+                                </div>
+                                <div class="actions">
+                                    <a href="#"> <img src="/assets/images/icon-edit.png"></a>
+                                    <a href="#"> <img src="/assets/images/icon-delete.png"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </main>
+            </div>
+        </div>
     </body>
 </html>
