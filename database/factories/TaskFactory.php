0d5b07d140b0843482fd7845cf9f9b6dec902807
Vinícius S. Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-02-26 00:03:04 -0300

    updated TaskFactory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -22,6 +22,7 @@ class TaskFactory extends Factory
         $category = Category::all()->random();
 
         return [
+            'is_done' => $this->faker->boolean(),
             'title' => $this->faker->text(30),
             'description' => $this->faker->text(60),
             'due_date' => $this->faker->dateTime(),
