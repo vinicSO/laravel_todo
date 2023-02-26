677f24bf2ef94425f47062dda799a1529ef5eca8
Vinícius S. Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-02-26 00:31:15 -0300

    create form to new task

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,5 +6,27 @@
         </a>
     </x-slot:btn>
 
-    Tela de Criar uma tarefa
+    <section id="create_task_section">
+        <h1>Criar Tarefa</h1>
+        <form>
+            <div class="input_area">
+                <label for="title">Título da tarefa</label>
+                <input id="title" name="title" type="text" placeholder="Digite o título da tarefa" required>
+            </div>
+            <div class="input_area">
+                <label for="due_date">Data de Realização</label>
+                <input id="due_date" name="due_date" type="date" required>
+            </div>
+            <div class="input_area">
+                <label for="category">Categoria</label>
+                <select id="category" name="category" required>
+                    <option selected disabled value>Selecione a categoria</option>
+                </select>
+            </div>
+            <div class="input_area">
+                <label for="description">Descrição da tarefa</label>
+                <textarea id="description" name="description" placeholder="Digite uma descrição para sua tarefa"></textarea>
+            </div>
+        </form>
+    </section>
 </x-layout>
