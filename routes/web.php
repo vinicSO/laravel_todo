a4092d9c72a4ad73aba317be0f9c4bf6a8307fb4
Vinícius S. Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-02-25 23:30:47 -0300

    created routes to views

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/task/new', [TaskController::class, 'create'])->name('task.create');
+Route::get('/task/{id}', [TaskController::class, 'show'])->name('task.show');
+
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
